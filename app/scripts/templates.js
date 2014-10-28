02c98f69cc5a334ee292b1b87a4b6cca7c400cda
Jason Gonzales <jason.gonzales23@gmail.com>
2014-10-27 20:52:33 -0700

    update grunt ember templates package and dependencies

--- app/scripts/templates.js
+++ app/scripts/templates.js
@@ -1,9 +1,1 @@
-Ember.TEMPLATES["index"] = Ember.Handlebars.template(function anonymous(Handlebars,depth0,helpers,partials,data) {
-this.compilerInfo = [4,'>= 1.0.0'];
-helpers = this.merge(helpers, Ember.Handlebars.helpers); data = data || {};
-  
-
-
-  data.buffer.push("<h1>uhhhh, what?</h1>\n");
-  
-});
+Ember.TEMPLATES["index"] = Ember.Handlebars.template([object Object]);
